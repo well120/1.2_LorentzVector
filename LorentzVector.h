cb5493409f9ea84d4a1e302ffc597309d1e5ab29
well120 <n.ushakova1@g.nsu.ru>
2019-10-27 18:40:22 +0700

    https://github.com/well120/1.2_LorentzVector

--- LorentzVector.h
+++ LorentzVector.h
@@ -1,4 +1,11 @@
+#include <iostream>
+
 class LorentzVector {
+private:
+    double t_;
+    double x_;
+    double y_;
+    double z_;
 public:
     LorentzVector();
     LorentzVector(double, double, double, double);
@@ -9,10 +16,10 @@ public:
     double z() const;
     double norm() const;
 
-    void t(double x);
-    void x(double x);
-    void y(double x);
-    void z(double x);
+    void t(double v);
+    void x(double v);
+    void y(double v);
+    void z(double v);
 
     void operator+=(const LorentzVector& other);
     void operator-=(const LorentzVector& other);
@@ -20,9 +27,9 @@ public:
     LorentzVector operator+(const LorentzVector& other) const;
     LorentzVector operator-(const LorentzVector& other) const;
     friend LorentzVector operator*(const LorentzVector& lv, double a);
-    friend std::ostream& operator << (std::ostream&, const LorentzVector&);
+    friend std::ostream& operator << (std::ostream& out, const LorentzVector& b);
     void zboost(double beta);
     double dot(const LorentzVector& other) const;
 };
 
-std::ostream& operator << (std::ostream&, const LorentzVector&);
+std::ostream& operator<< (std::ostream &out, const LorentzVector &b);
